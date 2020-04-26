fcc56aebf2c80c248bda593e1fe993d5ede31de1
wjw31891083 <31891083@qq.com>
2020-04-26 14:27:25 +0800

    up1

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -7,6 +7,7 @@ class IndexController extends BaseController
 {
     public function index()
     {
+        echo 2222;
         // echo date ( 'YmdHis' );die;
         // $m = new HistoryHearModel;
         // $o = $m->findFirst("id = '1a74471e0e1e4159bdbd9fd9ae21d27a'")->toArray();
